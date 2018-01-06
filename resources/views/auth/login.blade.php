1dec15e78d86ebea5c739f72fdb8b0688098e8b7
Arif Khan <arifkhan.incaendo@gmail.com>
2018-01-06 18:12:51 +0530

    SOC home page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero is-fullheight">
+<section class="hero soc-app-form">
     <div class="hero-body">
         <div class="container has-text-centered">
             <div class="column is-4 is-offset-4">
                 <h3 class="title has-text-grey">Login</h3>
                 <div class="box">
-                    <form method="POST" action="{{ route('login') }}">
+                    <form method="POST" id="login-form" action="{{ route('login') }}">
                         {{ csrf_field() }}                
                         <div class="field">
                             <div class="control has-icons-left has-icons-right">
@@ -49,11 +49,6 @@
                         </a>
                     </form>
                 </div>
-                <p>
-                    <a href="../" class="has-text-grey">Sign Up</a> &nbsp;·&nbsp;
-                    <a href="../" class="has-text-grey">Forgot Password</a> &nbsp;·&nbsp;
-                    <a href="../" class="has-text-grey">Need Help?</a>
-                </p>
             </div>
         </div>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero is-medium is-primary">
+<section class="hero is-blue is-bold">
   <div class="hero-body">
     <div class="container">
-      <h1 class="title">
-        Primary bold title
-      </h1>
-      <h2 class="subtitle">
-        Primary bold subtitle
-      </h2>
+      <div class="columns">
+        <div class="column">
+          <h5 class="title is-5">Mathematics</h5>
+        </div>
+        <div class="column">
+          <h5 class="title is-5">English</h5>
+        </div>
+        <div class="column">
+          <h5 class="title is-5">Reasoning</h5>
+        </div>
+        <div class="column">
+          <h5 class="title is-5">General Knowledge</h5>
+        </div>
+      </div>
     </div>
   </div>
 </section>
+<section class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <div class="columns">
+        <div class="column has-text-centered">
+          <h4 class="title is-4">Online Training & Preparation For Competitive Exams in India</h4>
+          <p class="subheading has-no-padding mb-1">Boost your exam preparation with SSCONLINECOACHING.COM</h5>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="testimonials is-medium is-white pt-2">
+  <div class="container">
+    <div class="columns has-text-centered-tablet">
+      <div class="column  is-offset-3-widescreen is-6-tablet is-offset-3-tablet is-vertically-centered">
+        <div>
+          <div class="wow bounceInRight ml-1">
+            <h4 class="title is-4 mb-1">
+              Thousands of students crack exams with SSCONLINECOACHING.COM
+            </h4>
+            <p class="subheading has-no-padding mb-1">
+              “Why should I join SSCONLINECOACHING.COM?” It's a fair enough question! Would
+              hundreds upon hundreds of glowing endorsements help?
+            </p> 
+            <a href="/testimonials" class="button is-primary is-padded">
+              Testimonials
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section> 
+            
 @endsection

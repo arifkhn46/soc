c3dda0abfb1d69df44e80b91dbfd2f5d97501738
ak-floweraura <arif.khan@bakingo.com>
2018-01-29 15:55:51 +0530

    A subject belongs to a content repository

---
+++ tests/Unit/Teacher/SubjectTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit\Teacher;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SubjectTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function a_subject_belongs_to_a_content_repository()
+    {
+        $subject = create('App\Subject');
+        $this->assertInstanceOf(
+            'Illuminate\Database\Eloquent\Collection',
+            $subject->contentRepositories
+        );
+    }
+}

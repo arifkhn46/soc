5a67daa5f59737f593172fc1c207a6b13d7be5c0
Arif Khan <arifkhan.incaendo@gmail.com>
2018-04-24 01:42:46 +0530

    Migration for contents

---
+++ database/migrations/2018_04_23_200836_create_contents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50)->unique();
+            $table->longText('description');
+            $table->integer('content_repo_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('content_repo_id')
+                ->references('id')->on('content_repositories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}

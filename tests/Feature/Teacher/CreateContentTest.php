f4185c07c433388a086acacf011a3602ba314e68
Arif Khan <arifkhan.incaendo@gmail.com>
2018-04-24 18:33:21 +0530

    Create Content Test for a teacher

---
+++ tests/Feature/Teacher/CreateContentTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Teacher;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateContent extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_teacher_can_add_a_content_to_his_content_repository()
+    {
+        $this->signInAsTeacher();
+        $contentRespository = create('App\ContentRepository');
+        $title = 'Maths';
+        $this->createContent(['title' => $title, 'content_repo_id' => $contentRespository->id]);
+        $this->assertDatabaseHas('contents', ['title' => $title]);
+    }
+
+    private function createContent($overrides = []) {
+        $content = make('App\Content', $overrides);
+        $response = $this->post(route('teacher.content.store'), $content->toArray());
+        return $response;
+    }
+}

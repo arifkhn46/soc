6e6aa9bdd005a8f3cbb0c7c9f93bb1f6228777cd
Arif Khan <arifkhan.incaendo@gmail.com>
2018-04-25 10:27:36 +0530

    Update field name

--- database/factories/ContentFactory.php
+++ database/factories/ContentFactory.php
@@ -6,7 +6,7 @@ $factory->define(App\Content::class, function (Faker $faker) {
     return [
         'title' => $faker->text(10),
         'description' => $faker->text(200),
-        'content_repo_id' => function () {
+        'content_repository_id' => function () {
             return factory('App\ContentRepository')->create()->id;
         },
     ];

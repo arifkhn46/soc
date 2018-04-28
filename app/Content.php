68105f5de93495ecdee8debdafd7fa5e4a0f7845
Arif Khan <arifkhan.incaendo@gmail.com>
2018-04-28 16:58:24 +0530

    Content and asset relationship

--- app/Content.php
+++ app/Content.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Asset;
 use Illuminate\Database\Eloquent\Model;
 
 class Content extends Model
@@ -14,4 +15,27 @@ class Content extends Model
     protected $fillable = [
         'title', 'description', 'content_repository_id',
     ];
+
+    /**
+     * A content may have many assets.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function assets()
+    {
+        return $this->belongsToMany(Asset::class, 'contents_assets');
+    }
+
+    /**
+     * Add an asset to the content.
+     *
+     * @param  array $asset
+     * @return Model
+     */
+    public function addAsset($asset)
+    {
+        $asset = $this->assets()->create($asset);
+        return $asset;
+    }
+
 }

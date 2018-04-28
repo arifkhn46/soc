2ac7d7118f2cd09bd1dd08dd29a284b1d11d22ca
Arif Khan <arifkhan.incaendo@gmail.com>
2018-04-28 16:58:53 +0530

    Content asset test

---
+++ tests/Unit/ContentTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContentTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $content;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->content = create(\App\Content::class);
+    }
+
+
+    /** @test */
+    public function a_content_has_assets()
+    {
+        $this->assertInstanceOf(
+            'Illuminate\Database\Eloquent\Collection',
+            $this->content->assets
+        );
+
+    }
+
+    /** @test */
+    public function a_content_can_add_an_asset()
+    {
+        $asset = make('App\Asset')->toArray();
+        $this->assertCount(0, $this->content->assets);
+        $this->content->addAsset($asset);
+        $this->assertCount(1, $this->content->fresh()->assets);
+    }
+
+}

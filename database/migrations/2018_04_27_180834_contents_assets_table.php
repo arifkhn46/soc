605597e85be40ed6c08fb501c33c6d811ca66956
Arif Khan <arifkhan.incaendo@gmail.com>
2018-04-28 16:59:22 +0530

    Pivot table of content and asset

---
+++ database/migrations/2018_04_27_180834_contents_assets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ContentsAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents_assets', function (Blueprint $table) {
+            $table->unsignedInteger('content_id');
+            $table->unsignedInteger('asset_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table');
+    }
+}

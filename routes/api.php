d17b7db22275aa32f199434e1c43d55e02dd7186
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-09 19:29:30 +0530

    User login api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function(){
+        Route::post('login', 'Api\Auth\LoginController@login')->name('api.user.login');
+        // Route::post('register', 'Api\RegisterController@register')->name('api.user.register');
+        // Route::group(['middleware' => 'auth:api'], function(){
+        //     Route::post('getUser', 'Api\AuthController@profile');
+        // });
+});
---
+++ tests/Feature/Api/UserAuthenticationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserAuthenticationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_user_can_login()
+    {
+        $this->withoutExceptionHandling();
+        $user = factory('App\User')->create();
+
+        $response = $this->json('POST', route('api.user.login'), [
+            'email'=> $user->email,
+            'password' => 'secret'
+        ])->assertStatus(200);
+            
+        // $this->assertArrayHasKey('access_token', $response->json());
+    }
+}

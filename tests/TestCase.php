172208b4718cb6bf097b44265c9e303cc2ed7af9
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-10 13:09:32 +0530

    Remove obsolete code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,6 +5,7 @@ namespace Tests;
 use App\Exceptions\Handler;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Laravel\Sanctum\Sanctum;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -36,23 +37,4 @@ abstract class TestCase extends BaseTestCase
         return $teacher;
     }
 
-    // protected function disableExceptionHandling()
-    // {
-    //     $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
-
-    //     $this->app->instance(ExceptionHandler::class, new class extends Handler {
-    //         public function __construct() {}
-    //         public function report(\Exception $e) {}
-    //         public function render($request, \Exception $e) {
-    //             throw $e;
-    //         }
-    //     });
-    // }
-
-    // protected function withExceptionHandling()
-    // {
-    //     $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
-
-    //     return $this;
-    // }
 }

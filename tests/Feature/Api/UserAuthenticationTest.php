5d77448a0cbb1448b21b4868cfee31519a0fcefd
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-10 13:10:09 +0530

    Make sure unauthenticated user can not access user profile

--- tests/Feature/Api/UserAuthenticationTest.php
+++ tests/Feature/Api/UserAuthenticationTest.php
@@ -4,17 +4,18 @@ namespace Tests\Feature\Api;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Tests\TestCaseApi;
 
-class UserAuthenticationTest extends TestCase
+class UserAuthenticationTest extends TestCaseApi
 {
     use RefreshDatabase;
 
     /** @test */
-    public function a_user_can_login()
+    public function a_user_can_get_an_access_token_on_login()
     {
-        $this->withoutExceptionHandling();
         $user = factory('App\User')->create();
+
+        $this->json('GET', route('api.user.profile'))->assertStatus(401);
 
         $response = $this->json('POST', route('api.user.login'), [
             'email'=> $user->email,
@@ -22,5 +23,9 @@ class UserAuthenticationTest extends TestCase
         ])->assertStatus(200);
 
         $this->assertArrayHasKey('access_token', $response->json());
+
+        $this->withHeaders([
+            'Authorization' => 'Bearer '. $response->json()['access_token'],
+        ])->json('GET', route('api.user.profile'))->assertStatus(200);
     }
 }

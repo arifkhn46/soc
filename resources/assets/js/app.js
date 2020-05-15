8fd01d64db5f70576d761265965b04b49596ed83
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-15 14:22:04 +0530

    Tailwind integration

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,14 +9,18 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import mixin from './plugins/mixin';
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
+Vue.component('login-form', require('./components/LoginForm.vue'));
+Vue.component('registration-form', require('./components/RegistrationForm.vue'));
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
 });
---
+++ resources/assets/js/plugins/mixin.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+Vue.mixin({
+  methods: {
+    validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+})

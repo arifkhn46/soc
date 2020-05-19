60317e46dbb23ef42e0b2c2185ab3008a01af797
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-20 04:33:43 +0530

    Mixin

---
+++ vue_app/student_app/src/mixin/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+var mixin = Vue.mixin({
+  methods: {
+    validEmail: function (email) {
+      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    }
+  }
+})
+
+export default mixin

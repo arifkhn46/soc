4f39aea32bd322ee9db09d10117344eba3cc5fa4
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-26 17:58:05 +0530

    Add task types and states to config

--- config/soc.php
+++ config/soc.php
@@ -9,4 +9,15 @@ return [
     // Add the email addresses of users who should be teacher here.
     'jyoti.raman2013@gmail.com'
   ],
+  'task_types' => [
+      [ 'id' => 1, 'name' => 'Theory'],
+      [ 'id' => 2, 'name' => 'Practical'],
+      [ 'id' => 3, 'name' => 'Mock']
+  ],
+  'task_states' => [
+    ['id' => 1, 'name' => 'Created'],
+    ['id' => 2, 'name' => 'In progress'],
+    ['id' => 3, 'name' => 'On hold'],
+    ['id' => 4, 'name' => 'Completed']
+  ],
 ];

c7ae80dedea14c752126209a15a7a06a3898fffc
Arif Khan <arifkhan.incaendo@gmail.com>
2020-05-26 18:00:19 +0530

    Register api new paths

--- routes/api.php
+++ routes/api.php
@@ -21,9 +21,13 @@ Route::prefix('v1')->group(function(){
         Route::post('login', 'Api\Auth\LoginController@login')->name('api.user.login');
         // Route::post('register', 'Api\RegisterController@register')->name('api.user.register');
         Route::group(['middleware' => 'auth:sanctum'], function(){
-            Route::post('task', 'Api\TaskController@store')->name('api.task.create');
+            Route::post('tasks', 'Api\TaskController@store')->name('api.task.create');
+            Route::get('tasks', 'Api\TaskController@index')->name('api.task.my_tasks');
             Route::get('/user', function (Request $request) {
-                    return $request->user();
+                    return new App\Http\Resources\UserResource($request->user());
                 })->name('api.user.profile');
+            Route::get('subjects', 'Api\SubjectController@details')->name('api.subject.list');
+            Route::get('subjects/{subject}/chapters', 'Api\SubjectController@chapters')->name('api.subject.chapters');
+            
         });
 });
